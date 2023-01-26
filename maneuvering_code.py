74fea8c9182e0a8a974bef614220b1fd2bb8bf25
Zachary Lee <zacharyianlee@gmail.com>
2023-01-26 13:25:09 -0800

    Halved used speed in maneuvering code

--- maneuvering_code.py
+++ maneuvering_code.py
@@ -2,9 +2,8 @@ from picarx_improved import Picarx
 import time
 
 
-def simple_move(car, dist, dir, angle):
+def simple_move(car, dist, dir, angle, speed):
     car.set_dir_servo_angle(angle)
-    speed = 200
     time_to_sleep = abs(dist)/speed
 
     if dir == 0:
@@ -23,8 +22,8 @@ def k_turn(car):
 
 if __name__ == "__main__":
     px = Picarx()
-    simple_move(px, 100, 0, 0)
-    simple_move(px, 100, 1, 0)
-    simple_move(px, 100, 0, 35)
-    simple_move(px, 100, 0, -35)
+    simple_move(px, 100, 0, 0, 100)
+    simple_move(px, 100, 1, 0, 100)
+    simple_move(px, 100, 0, 35, 100)
+    simple_move(px, 100, 0, -35, 100)
     px.stop()

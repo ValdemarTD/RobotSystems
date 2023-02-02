45839efe1d4e6ac532de8889b2cc8375cff313c2
Zachary Lee <zacharyianlee@gmail.com>
2023-02-02 12:32:52 -0800

    Added greyscale class

---
+++ greyscale_class.py
@@ -0,0 +1,31 @@
+try:
+    from ezblock import ADC
+except:
+    from robot_hat import ADC
+
+import time
+import logging
+
+
+logging_format = "%(asctime)s : %(message)s "
+logging.basicConfig( format = logging_format, level = logging.DEBUG, datefmt ="%H:%M:%S")
+logging.getLogger().setLevel( logging.DEBUG )
+
+class GreyscaleSensor():
+    def __init__(self):
+        self.adc0 = ADC("A0")
+        self.adc1 = ADC("A1")
+        self.adc2 = ADC("A2")
+        self.val_list = [0, 0, 0]
+    
+    def query_sensor(self):
+        self.val_list = [self.adc0.read(), self.adc1.read(), self.adc2.read()]
+        logging.debug(f"Greyscale Sensor returned {self.val_list}")
+        return val_list
+
+
+if __name__=="__main__":
+    sensor = GreyscaleSensor()
+    while True:
+        sensor.query_sensor()
+        time.sleep(0.1)
